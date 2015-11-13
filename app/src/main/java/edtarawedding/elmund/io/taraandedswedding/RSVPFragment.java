ed7759e1bc91acf5200de46668a8c3998d4696da
Edmund Dipple <elmundio1987@gmail.com>
2015-11-13 16:38:43 +0000

    Initial attempt at loading webview

--- app/src/main/java/edtarawedding/elmund/io/taraandedswedding/RSVPFragment.java
+++ app/src/main/java/edtarawedding/elmund/io/taraandedswedding/RSVPFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import android.util.Log;
 
 /**
@@ -22,8 +24,23 @@ public class RSVPFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_rsvp , container, false);
         WebView mWebView = (WebView) v.findViewById(R.id.webView);
-        mWebView.loadUrl("https://www.google.com");
-        Log.i("MyActivity", "lol ");
+
+        //mWebView.getSettings().setJavaScriptEnabled(true);
+
+
+        mWebView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                return false;
+            }
+        });
+
+        mWebView.loadUrl("http://www.google.com/");
+
+
+
         return inflater.inflate(R.layout.fragment_rsvp, container, false);
     }
 }
