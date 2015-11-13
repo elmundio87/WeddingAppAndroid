e5c7228e946f4018586739b2bfd09fe78bef6349
Edmund Dipple <elmundio1987@gmail.com>
2015-11-13 18:36:06 +0000

    Implement map view with zoom

--- app/src/main/java/edtarawedding/elmund/io/taraandedswedding/MapFragment.java
+++ app/src/main/java/edtarawedding/elmund/io/taraandedswedding/MapFragment.java
@@ -1,10 +1,14 @@
 package edtarawedding.elmund.io.taraandedswedding;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,27 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+
+
+        String url = "file:///android_asset/map.html";
+
+        View rootView = inflater.inflate(R.layout.fragment_rsvp, container, false);
+
+        WebView wbvBrowser = (WebView) rootView.findViewById(R.id.webView);
+
+        wbvBrowser.getSettings().setJavaScriptEnabled(true);
+        wbvBrowser.getSettings().setBuiltInZoomControls(true);
+        wbvBrowser.getSettings().setDisplayZoomControls(false);
+
+        wbvBrowser.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        wbvBrowser.loadUrl(url);
+
+        return rootView;
     }
 }
